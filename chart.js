01846bbbd1bb4d306b51fab8e57ea0dc8b6ed4b4
Kiyohiko Heima <piyo326@gmail.com>
2015-06-17 00:49:40 +0900

    fix

--- chart.js
+++ chart.js
@@ -1,153 +1,147 @@
-(function(global){ 
+(function(global){
+
     global.createChart = createChart;
 
     function createChart() {
 	    this.datas = [];
     }
 
+    // datastoreのデータを、描画用のデータに変換する
+    function get_graph_data(data){
+      return data.map(function(d) {
+        return {
+          date : new Date(d.timestamp),
+          value : d.value
+        };
+      });
+    }
+
+    // グラフを描画するdivのidを指定する
     createChart.prototype.setSvg = function(wrapper_id) {
     	this.wrapper_id =  wrapper_id || 'svg-chart';
     }
 
+    // 描画するデータをセット／更新
     createChart.prototype.setDatas = function(datas) {
     	this.datas = datas;
     }
 
-    createChart.prototype.setDatastore = function(datastore) {
-    	function checkKey(elem) {
-    		var values = elem.value;
-    		for(var k in values) {
-    			if(typeof values[k] == "number") {
-    				return k;
-    			}
-    		}
-    		return null;
-    	}
-    	var key = checkKey(datastore[0]);
-    	var datas = datastore.map(function(e) {
-    		var values = e.value;
-    		return { timestamp : e.timestamp, value : values[key]};
-    	});
-    	this.setDatas(datas);
+    // 初期化
+    createChart.prototype.init = function() {
+
+      // 描画範囲に関する変数
+  		var margin = {top: 20, right: 20, bottom: 30, left: 50},
+  		    width = 1040 - margin.left - margin.right,
+  		    height = 420 - margin.top - margin.bottom;
+
+      // x軸のスケール（時間）。レンジ(出力範囲)の指定
+  		var xScale = d3.time.scale()
+  		    .range([0, width]);
+
+      // y軸のスケール（センサーデータの値）。レンジ(出力範囲)の指定
+  		var yScale = d3.scale.linear()
+  		    .range([height, 0]);
+
+      // スケールを元にx軸の設定（入力値の範囲はまだ指定していない。データを受け取ってから指定する）
+  		var xAxis = d3.svg.axis()
+  		    .scale(xScale)
+  		    .orient("bottom");
+
+      // スケールを元にy軸の設定
+  		var yAxis = d3.svg.axis()
+  		    .scale(yScale)
+  		    .orient("left");
+
+      // SVG要素の作成（attrとかはテンプレ）
+      var svg = d3.select("#" + this.wrapper_id).append("svg")
+          .attr("class", "chart")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // 折れ線グラフの設定。xに時間、yにセンサーデータの値を設定。
+  		var line = d3.svg.line()
+  		    .x(function(d) {
+            // xスケールでマップされた時間を返す
+  		    	return xScale(d.date);
+  		    })
+  		    .y(function(d) {
+            // yスケールでマップされたセンサーデータの値を返す
+  		    	return yScale(d.value);
+  		    });
+
+
+      // もろもろをメンバ変数に
+      this.height = height;
+      this.margin = margin;
+      this.width = width;
+      this.xScale = xScale;
+      this.yScale = yScale;
+      this.xAxis = xAxis;
+      this.yAxis = yAxis;
+      this.svg = svg;
+      this.line = line;
     }
 
-    createChart.prototype.height = function(v) {
-    	if(v) {
-    		var svgElement = document.getElementById(this.elem_id);
-    		svgElement.setAttribute("height", v);
-    	}else{
-    		var svgElement = document.getElementById(this.elem_id);
-    		var h = svgElement.getAttribute("height");
-    		return h.substr(0, h.length - 2);
-    	}
-    }
-    createChart.prototype.draw = function() {
-		var margin = {top: 20, right: 20, bottom: 30, left: 50},
-		    width = 840 - margin.left - margin.right,
-		    height = 320 - margin.top - margin.bottom;
 
-		var parseDate = d3.time.format("%y-%m-%d-%H");
+    // 最初の描画
+    createChart.prototype.initialDraw = function() {
 
-		var xScale = d3.time.scale()
-		    .range([0, width]);
+      var dataset = get_graph_data(this.datas);
 
-		var y = d3.scale.linear()
-		    .range([height, 0]);
+      // ドメイン（入力値の範囲）の設定、extentメソッドでdatasetの最小と最大を返す
+      this.xScale.domain(d3.extent(dataset, function(d) { return d.date; }));
+      this.yScale.domain(d3.extent(dataset, function(d) { return d.value; }));
 
-	    this.xScale = xScale;
-	    this.y = y;
+      // x軸の描画
+      this.svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + this.height + ")")
+          .call(this.xAxis);
 
-		var xAxis = d3.svg.axis()
-		    .scale(xScale)
-		    .orient("bottom");
+      // y軸の描画
+      this.svg.append("g")
+          .attr("class", "y axis")
+          .call(this.yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Value");
 
-		var yAxis = d3.svg.axis()
-		    .scale(y)
-		    .orient("left");
-
-	    this.xAxis = xAxis;
-	    this.yAxis = yAxis;
-
-		var line = d3.svg.line()
-		    .x(function(d) {
-		    	return xScale(d.date);
-		    })
-		    .y(function(d) {
-		    	return y(d.value);
-		    });
-
-		this.line = line;
-
-		var svg = d3.select("#" + this.wrapper_id).append("svg")
-		    .attr("class", "chart")
-		    .attr("width", width + margin.left + margin.right)
-		    .attr("height", height + margin.top + margin.bottom)
-		  .append("g")
-		    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-		this.svg = svg;
-
-		function get_data(data) {
-		  var data2 = data.map(function(d) {
-		  	return {
-		  		date : new Date(d.timestamp),
-		  		value : d.value
-		  	};
-		  });
-
-		  xScale.domain(d3.extent(data2, function(d) { return d.date; }));
-		  y.domain(d3.extent(data2, function(d) { return d.value; }));
-
-		  svg.append("g")
-		      .attr("class", "x axis")
-		      .attr("transform", "translate(0," + height + ")")
-		      .call(xAxis);
-
-		  svg.append("g")
-		      .attr("class", "y axis")
-		      .call(yAxis)
-		    .append("text")
-		      .attr("transform", "rotate(-90)")
-		      .attr("y", 6)
-		      .attr("dy", ".71em")
-		      .style("text-anchor", "end")
-		      .text("Value");
-
-		  svg.append("path")
-		      .datum(data2)
-		      .attr("class", "line")
-		      .attr("d", line);
-		}
-
-    	get_data(this.datas);
+      // 折れ線の描画
+      this.svg.append("path")
+          .datum(dataset)
+          .attr("class", "line")
+          .attr("d", this.line);
     }
 
-    createChart.prototype.update = function() {
-		var data2 = this.datas.map(function(d) {
-			return {
-				date : new Date(d.timestamp),
-				value : d.value
-			};
-		});
 
-		this.xScale.domain(d3.extent(data2, function(d) { return d.date; }));
-		this.y.domain(d3.extent(data2, function(d) { return d.value; }));
+    // 更新した際の描画
+    createChart.prototype.updateDraw = function() {
 
-		// Select the section we want to apply our changes to
-		this.svg = d3.select("#" + this.wrapper_id).transition();
+      var dataset = get_graph_data(this.datas);
 
-		// Make the changes
-		this.svg.select(".line")   // change the line
-		    .duration(750)
-		    .attr("d", this.line(data2));
-		this.svg.select(".x.axis") // change the x axis
-		    .duration(750)
-		    .call(this.xAxis);
-		this.svg.select(".y.axis") // change the y axis
-		    .duration(750)
-		    .call(this.yAxis);
+       // ドメイン（入力値の範囲）の更新
+  		this.xScale.domain(d3.extent(dataset, function(d) { return d.date; }));
+  		this.yScale.domain(d3.extent(dataset, function(d) { return d.value; }));
 
+  		// アニメーションしますよ、という宣言
+  		this.svg = d3.select("#" + this.wrapper_id).transition();
+
+  		this.svg.select(".line")   // 折れ線を
+  		    .duration(750) // 750msで
+  		    .attr("d", this.line(dataset)); // （新しい）datasetに変化させる描画をアニメーション
+
+  		this.svg.select(".x.axis") // x軸を
+  		    .duration(750) // 750msで
+  		    .call(this.xAxis); // （domainの変更によって変化した）xAxisに変化させる描画をアニメーション
+
+  		this.svg.select(".y.axis") // y軸を
+  		    .duration(750) // 750msで
+  		    .call(this.yAxis); // （domainの変更によって変化した）yAxisに変化させる描画をアニメーション
     }
 
-  
-  
 }(window))
